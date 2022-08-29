f24561c71dc3f819e48f51428339af35580eaab6
hhtjo <hahotjo@gmail.com>
2022-08-29 17:53:02 +0200

    Update public path

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -11,7 +11,7 @@ const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
-        publicPath: '/'
+        publicPath: '/github-actions-workshop/'
     },
     plugins: [
         new HtmlWebpackPlugin({
